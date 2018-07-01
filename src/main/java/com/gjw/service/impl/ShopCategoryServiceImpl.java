812db6ed7800b99b2fa5c788f831fa3b044fe8d9
gjw199513@163.com <gjw605134015>
2018-07-02 00:24:32 +0800

    项目设计和框架搭建+Logback配置与使用+店铺注册功能模块+店铺编辑和列表功能+商品类别模块+商品模块+前端展示系统

--- src/main/java/com/gjw/service/impl/ShopCategoryServiceImpl.java
+++ src/main/java/com/gjw/service/impl/ShopCategoryServiceImpl.java
@@ -1,11 +1,16 @@
 package com.gjw.service.impl;
 
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gjw.cache.JedisUtil;
 import com.gjw.dao.ShopCategoryDao;
 import com.gjw.entity.ShopCategory;
 import com.gjw.service.ShopCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,9 +20,32 @@ import java.util.List;
 public class ShopCategoryServiceImpl implements ShopCategoryService {
 
     @Autowired
+    private JedisUtil.Strings jedisStrings;
+    @Autowired
+    private JedisUtil.Keys jedisKeys;
+    @Autowired
     private ShopCategoryDao shopCategoryDao;
 
-    public List<ShopCategory> getShopCategoryList(ShopCategory shopCategoryCondition) {
-        return shopCategoryDao.queryShopCategory(shopCategoryCondition);
+    private static String SCLISTKEY = "shopcategorylist";
+
+    public List<ShopCategory> getShopCategoryList(ShopCategory shopCategoryCondition) throws IOException {
+        String key = SCLISTKEY;
+        List<ShopCategory> shopCategoryList = null;
+        ObjectMapper mapper = new ObjectMapper();
+        if (!jedisKeys.exists(key)) {
+//			ShopCategory shopCategoryCondition = new ShopCategory();
+//			shopCategoryCondition.setParentId(parentId);
+//			shopCategoryList = shopCategoryDao
+//					.queryShopCategory(shopCategoryCondition);
+//			String jsonString = mapper.writeValueAsString(shopCategoryList);
+//			jedisStrings.set(key, jsonString);
+        } else {
+            String jsonString = jedisStrings.get(key);
+            JavaType javaType = mapper.getTypeFactory()
+                    .constructParametricType(ArrayList.class,
+                            ShopCategory.class);
+            shopCategoryList = mapper.readValue(jsonString, javaType);
+        }
+        return shopCategoryList;
     }
 }
